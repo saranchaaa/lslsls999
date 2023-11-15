b8f2e034211c0dbc66a0ca697b9537d737ec5fa8
saranchaaa <visna489@gmail.com>
2023-11-15 17:34:03 +0200

    2

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import sqlite3 as sq
 cn = sq.connect("database1.sl3")
 cur = cn.cursor()
 # cur.execute("CREATE TABLE tb1(name TEXT)")
-cur.execute("INSERT INTO tb1 (name) VALUES ('j')")
-cur.execute("INSERT INTO tb1 (name) VALUES ('з')")
-cur.execute("INSERT INTO tb1 (name) VALUES ('5')")
+# cur.execute("INSERT INTO tb1 (name) VALUES ('j')")
+# cur.execute("INSERT INTO tb1 (name) VALUES ('з')")
+# cur.execute("INSERT INTO tb1 (name) VALUES ('5')")
+
+cur.execute("SELECT rowid, name FROM tb1")
 cn.commit()
 
-print(cur)
-print(cn)
+for i in cur.fetchall():
+    print(i)
 
 cn.close()
